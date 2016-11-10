438476dd2764fa494194c723a0d1f75c67fe8ec5
Heath Dutton ☕ <heathdutton@gmail.com>
2016-11-09 22:45:21 -0500

    Load cognizence

--- js/main.js
+++ js/main.js
@@ -32,17 +32,19 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 
-  setTimeout(function(){
-    $('.user-image').addClass('animated tada');
-    var del = 0;
+  $(document).load(function(){
     setTimeout(function(){
-      $('.navigation li').each(function(){
-        var t = $(this);
-        del += 100;
-        setTimeout(function(){
-          t.toggleClass('animated bounce');
-        }, del);
-      });
-    }, 300);
-  }, 3000);
+      $('.user-image').addClass('animated tada');
+      var del = 0;
+      setTimeout(function(){
+        $('.navigation li').each(function(){
+          var t = $(this);
+          del += 100;
+          setTimeout(function(){
+            t.toggleClass('animated bounce');
+          }, del);
+        });
+      }, 300);
+    }, 2000);
+  });
 })

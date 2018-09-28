3d08e97d8fae332cbab79598737aae614a7449ca
Heath Dutton ☕ <hdutton@thedmsgrp.com>
2018-09-27 22:40:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,21 +6,21 @@ $(document).ready(function () {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      // $('.panel-cover').addClass('panel-cover--collapsed')
+      // $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   });
 
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
+  //if (window.location.hash && window.location.hash == '#blog') {
+  //  $('.panel-cover').addClass('panel-cover--collapsed');
+  //}
 
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
+  //if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+  //  $('.panel-cover').addClass('panel-cover--collapsed');
+  //}
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
